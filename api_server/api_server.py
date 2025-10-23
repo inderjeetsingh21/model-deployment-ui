53d0c6b497d45e8e00400a07cab397ddf20438bd
inderjeetsingh21 <101826035+inderjeetsingh21@users.noreply.github.com>
2025-10-23 11:17:04 -0700

    New version with port 8001

--- api_server/api_server.py
+++ api_server/api_server.py
@@ -514,7 +514,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api_server:app",
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=True,
         log_level="info"
     )
